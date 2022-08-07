b6ee46af721eb12c500c4e57e7112e039e7061d3
online135 <b97b01067@ntu.edu.tw>
2022-08-07 17:18:08 +0800

    index, footer raw setting

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -448,10 +448,9 @@
             </fieldset>
             <!-- end ngIf: row.type == 'radio' -->
 
-
             <!-- ngIf: row.type == 'textarea' -->
-            <!-- ngIf: row.type == 'radio' -->
-            <!-- ngIf: row.type == 'select' -->
+            <textarea style="background:#FFFFFF" class="form-control ng-pristine ng-untouched ng-scope ng-invalid ng-invalid-required" ng-if="row.type == 'textarea'" ng-model="row.answer" ng-required="row.required == '1'" required="required"></textarea>
+            <!-- ngIf: row.type == 'textarea' -->
           </td>
           <td ng-show="confirm" class="ng-binding ng-hide"></td>
         </tr>
@@ -491,29 +490,7 @@
 	</div>
 
 
-
-
-	
-<div class="section container-fluid">
-  <div class="row">
-        <div class="col-xs-120"><a href="https://glbbestjapan.com/"><img src="https://glbbestjapan.com/wp-content/uploads/2020/01/GLBrogo.png" alt="Global Best Japan" title="Global Best Japan" width="200" height="200" class="footer_logo"></a></div>    <div class="col-xs-120 footer-menu">
-          </div>
-  </div>
-</div>
-
-<div class="text-center returntop">
-	<a href="#verytop"><span></span></a>
-</div>
-
-<div style="background:black">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-30 text-right col-sm-push-90">
-                        <a class="target_blank" href="https://glbbestjapan.com/feed/" target="_blank"><img src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/images/rss.png" class="social-icon"></a>			</div>
-      <div class="col-sm-offset-30 col-sm-60 col-sm-pull-30 text-center romaji" style="color:white; height:60px; line-height:60px;">Copyright 2022 Global Best Japan</div>
-		</div>
-	</div>
-</div>
+@include('layouts.footer');
 
 <script type="text/javascript" src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/includes/js/skip-link-focus-fix.js?ver=20130115" id="_tk-skip-link-focus-fix-js"></script>
 <script type="text/javascript" src="https://glbbestjapan.com/wp-includes/js/wp-embed.min.js?ver=5.7.1" id="wp-embed-js"></script>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<div class="section container-fluid">
+  <div class="row">
+        <div class="col-xs-120"><a href="https://glbbestjapan.com/"><img src="https://glbbestjapan.com/wp-content/uploads/2020/01/GLBrogo.png" alt="Global Best Japan" title="Global Best Japan" width="200" height="200" class="footer_logo"></a></div>    <div class="col-xs-120 footer-menu">
+          </div>
+  </div>
+</div>
+
+<div class="text-center returntop">
+	<a href="#verytop"><span></span></a>
+</div>
+
+<div style="background:black">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-30 text-right col-sm-push-90">
+                        <a class="target_blank" href="https://glbbestjapan.com/feed/" target="_blank"><img src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/images/rss.png" class="social-icon"></a>			</div>
+      <div class="col-sm-offset-30 col-sm-60 col-sm-pull-30 text-center romaji" style="color:white; height:60px; line-height:60px;">Copyright 2022 Global Best Japan</div>
+		</div>
+	</div>
+</div>
