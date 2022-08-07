27041bae8b2c986fbf2d76723f2519ebb3d74e7a
online135 <b97b01067@ntu.edu.tw>
2022-08-07 22:46:01 +0800

    create head.blade.php and put all into it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,265 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head><style type="text/css">@charset "UTF-8";[ng\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-	<title>Global Best Japan | 「IT×金融というサービス提供を通して世界の発展へ！」「グローバルベストジャパン株式会社」は、金融業界向けにシステムやサービスを提供する会社です。2014年に香港で設立されて2019年に日本進出を果たしました。現在は香港・シンガポール・東京の3都市にオフィスを構えています。</title>
-	<meta name="description" content="「IT×金融というサービス提供を通して世界の発展へ！」「グローバルベストジャパン株式会社」は、金融業界向けにシステムやサービスを提供する会社です。2014年に香港で設立されて2019年に日本進出を果たしました。現在は香港・シンガポール・東京の3都市にオフィスを構えています。">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="https://glbbestjapan.com/xmlrpc.php">
-
-		<meta name="robots" content="max-image-preview:large">
-<link rel="dns-prefetch" href="//ajax.googleapis.com">
-<link rel="dns-prefetch" href="//s.w.org">
-<link rel="alternate" type="application/rss+xml" title="Global Best Japan » フィード" href="https://glbbestjapan.com/feed/">
-<link rel="alternate" type="application/rss+xml" title="Global Best Japan » コメントフィード" href="https://glbbestjapan.com/comments/feed/">
-		<script type="text/javascript">
-			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/glbbestjapan.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.7.1"}};
-			!function(e,a,t){var n,r,o,i=a.createElement("canvas"),p=i.getContext&&i.getContext("2d");function s(e,t){var a=String.fromCharCode;p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,e),0,0);e=i.toDataURL();return p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,t),0,0),e===i.toDataURL()}function c(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(o=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},r=0;r<o.length;r++)t.supports[o[r]]=function(e){if(!p||!p.fillText)return!1;switch(p.textBaseline="top",p.font="600 32px Arial",e){case"flag":return s([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])?!1:!s([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!s([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]);case"emoji":return!s([55357,56424,8205,55356,57212],[55357,56424,8203,55356,57212])}return!1}(o[r]),t.supports.everything=t.supports.everything&&t.supports[o[r]],"flag"!==o[r]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[o[r]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(n=t.source||{}).concatemoji?c(n.concatemoji):n.wpemoji&&n.twemoji&&(c(n.twemoji),c(n.wpemoji)))}(window,document,window._wpemojiSettings);
-		</script><script src="https://glbbestjapan.com/wp-includes/js/wp-emoji-release.min.js?ver=5.7.1" type="text/javascript" defer=""></script>
-		<style type="text/css">
-img.wp-smiley,
-img.emoji {
-	display: inline !important;
-	border: none !important;
-	box-shadow: none !important;
-	height: 1em !important;
-	width: 1em !important;
-	margin: 0 .07em !important;
-	vertical-align: -0.1em !important;
-	background: none !important;
-	padding: 0 !important;
-}
-</style>
-	<link rel="stylesheet" id="wp-block-library-css" href="https://glbbestjapan.com/wp-includes/css/dist/block-library/style.min.css?ver=5.7.1" type="text/css" media="all">
-<link rel="stylesheet" id="tcd-contact-form-css" href="https://glbbestjapan.com/wp-content/plugins/tcd-workflow/tcd-contact-admin.css?ver=5.7.1" type="text/css" media="all">
-<link rel="stylesheet" id="_tk-bootstrap-wp-css" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/includes/css/bootstrap-wp.css?ver=5.7.1" type="text/css" media="all">
-<link rel="stylesheet" id="_tk-bootstrap-css" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/includes/resources/bootstrap/css/bootstrap.min.css?ver=5.7.1" type="text/css" media="all">
-<link rel="stylesheet" id="_tk-font-awesome-css" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/includes/css/font-awesome.min.css?ver=4.1.0" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-icons-css" href="https://glbbestjapan.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.15.0" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-frontend-legacy-css" href="https://glbbestjapan.com/wp-content/plugins/elementor/assets/css/frontend-legacy.min.css?ver=3.6.7" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-frontend-css" href="https://glbbestjapan.com/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=3.6.7" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-post-134-css" href="https://glbbestjapan.com/wp-content/uploads/elementor/css/post-134.css?ver=1657700470" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-global-css" href="https://glbbestjapan.com/wp-content/uploads/elementor/css/global.css?ver=1657700484" type="text/css" media="all">
-<link rel="stylesheet" id="google-fonts-1-css" href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;display=auto&amp;ver=5.7.1" type="text/css" media="all">
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js?ver=5.7.1" id="jquery-js"></script>
-<script type="text/javascript" src="https://glbbestjapan.com/wp-content/plugins/tcd-workflow/angular.js?ver=5.7.1" id="angular-js"></script>
-<script type="text/javascript" src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/includes/resources/bootstrap/js/bootstrap.js?ver=5.7.1" id="_tk-bootstrapjs-js"></script>
-<script type="text/javascript" src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/includes/js/bootstrap-wp.js?ver=5.7.1" id="_tk-bootstrapwp-js"></script>
-<link rel="https://api.w.org/" href="https://glbbestjapan.com/wp-json/"><link rel="alternate" type="application/json" href="https://glbbestjapan.com/wp-json/wp/v2/pages/18"><link rel="canonical" href="https://glbbestjapan.com/">
-<link rel="shortlink" href="https://glbbestjapan.com/">
-<link rel="alternate" type="application/json+oembed" href="https://glbbestjapan.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fglbbestjapan.com%2F">
-<link rel="alternate" type="text/xml+oembed" href="https://glbbestjapan.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fglbbestjapan.com%2F&amp;format=xml">
-<style type="text/css"></style>	<style type="text/css">
-			.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-		</style>
-	<link rel="icon" href="https://glbbestjapan.com/wp-content/uploads/2021/05/cropped-名称未設定のデザイン-50-32x32.png" sizes="32x32">
-<link rel="icon" href="https://glbbestjapan.com/wp-content/uploads/2021/05/cropped-名称未設定のデザイン-50-192x192.png" sizes="192x192">
-<link rel="apple-touch-icon" href="https://glbbestjapan.com/wp-content/uploads/2021/05/cropped-名称未設定のデザイン-50-180x180.png">
-<meta name="msapplication-TileImage" content="https://glbbestjapan.com/wp-content/uploads/2021/05/cropped-名称未設定のデザイン-50-270x270.png">
-		<style type="text/css" id="wp-custom-css">
-			.agent-header-menu .navbar{
-margin-left:0px;
-}
-
-#logo-area img{
-width:60px;
-}
-		</style>
-			<link rel="stylesheet" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/style.css?ver=3.1.16" type="text/css">
-
-	
-	<script type="text/javascript" src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/parallax.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/agent.css" type="text/css">
-
-		<script type="text/javascript" src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/js/jscript.js"></script>
-	<script src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/js/comment.js?ver=?ver=3.1.16"></script>
-	<link rel="stylesheet" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/comment-style.css" type="text/css">
-
-	<link rel="stylesheet" media="screen and (max-width:770px)" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/footer-bar/footer-bar.css?ver=?ver=3.1.16">
-
-		<!-- carousel -->
-		<script type="text/javascript" src="https://glbbestjapan.com/wp-content/themes/agent_tcd033/js/slick.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://glbbestjapan.com/wp-content/themes/agent_tcd033/js/slick.css">
-	<script type="text/javascript" charset="utf-8">
-		jQuery(window).load(function() {
-			jQuery('.carousel').slick({
-				arrows: false,
-				dots: false,
-				infinite: true,
-				slidesToShow: 5,
-				slidesToScroll: 1,
-				autoplay: true,
-				autoplaySpeed: 3000,
-				responsive: [
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 4
-						}
-					},
-					{
-						breakpoint: 600,
-						settings: {
-							slidesToShow: 3
-						}
-					},
-					{
-						breakpoint: 480,
-						settings: {
-							slidesToShow: 3
-						}
-					}					
-				]
-			});
-		});
-	</script>
-	<!-- /carousel -->
-	
-	<style type="text/css">
-			</style>
-
-	<style type="text/css">
-		#logo_image { top:0px; left:0px; }
-		body { font-size:14px; }
-	.menu-item a:hover, .entry-content a {color: #3FA5F5 !important;}
-	.title a:hover, .entry-meta a:hover, .post_category a:hover, .widget a:hover, .categories-wrap a:hover {color: #3FA5F5!important;}
-	.dropdown-menu {
-	  background-color:#3FA5F5;
-	}
-	.dropdown-menu .menu-item:hover *{
-	  background:#2C73AB!important;
-	}
-	.link-button-azure_top, .link-button-azure_btm{
-	  background-color:#3FA5F5!important;
-	}
-	.next-button:after { border-left-color:3FA5F5!important; border-bottom-color:3FA5F5!important; }
-	.next-button:hover, .next-button:hover:after {border-color:2C73AB!important;}
-	.link-button-azure_top:hover, .link-button-azure_btm:hover {
-	  background:#2C73AB!important;
-	}
-	.read-more:hover, .archives_btn:hover, .prev_link a:hover, .next_link a:hover, #submit_comment:hover, .form_table .btn-primary, .form_table td.form_title  { background:#3FA5F5!important; }
-
-  @media only screen and (min-width: 767px) {
-    .navbar-nav > .current-menu-item > a{color: #3FA5F5 !important;}
-  }
-  
-  @media only screen and (max-width: 767px) {
-    .current-menu-item *{color: #3FA5F5 !important;}
-    .menu-item.active a {
-      color:black;
-    }
-  }
-
-			p { word-wrap:break-word; }
-	
-	@media screen and (max-width: 640px) {
-
-#logo_image img {
-    display: none;
-}
-
-#logo_image a:before {
-    content: attr(title);
-}
-
-}
-
-#agent-splash-text{
-width: 100%;
-    margin-top: 60px;
-    padding: 50px 50px;
-    background-color: rgba(0, 0, 0, 0.6);
-  
- 　line-height: 1.6;
-}
-
-	
-			#agent-header-outer{ position: fixed;}
-		.navbar-collapse.collapse.in{ margin-top:0; }
-		.mobile_nav_wapper{ position:fixed; top:90px; }
-	
-
-			.top_content4_post .post-meta .title{ font-size: 0px;}
-		#agent-splash-text{
-			color: #E3E3E3;
-			text-shadow: 0px 0px 5px #0C023B !important;
-		}
-		.next-button{
-			background-color: #FFFFFF;
-			color: #3FA5F5;
-		}
-		.next-button:after {
-			border-left: 3px solid #3FA5F5;
-			border-bottom: 3px solid #3FA5F5;
-		}
-				#section-3 .liner{ font-size: 40px; }
-		#section-3 .lead{ font-size: 21px; }
-		#section-4 .liner{ font-size: 0px; }
-		#section-4 .lead{ font-size: 0px; }
-		#section-5 .liner{ font-size: 0px; }
-		#section-5 .lead{ font-size: 0px; }
-		@media screen and (max-width: 769px){
-			#section-3 .liner{ font-size: 20px; }
-			#section-3 .lead{ font-size: 16px; }
-			#section-4 .liner{ font-size: 0px; }
-			#section-4 .lead{ font-size: 0px; }
-			#section-5 .liner{ font-size: 0px; }
-			#section-5 .lead{ font-size: 0px; }
-		}
-							.form_table td.form_title{ background: #3FA5F5; }
-		.form_table .btn-primary{ background: #3FA5F5; }
-		.form_table .btn-primary:hover{ background: #2C73AB; }
-	
-	</style>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-WW4M66SGYB"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WW4M66SGYB');
-</script>
-<style type="text/css">
-@font-face {
-  font-weight: 400;
-  font-style:  normal;
-  font-family: 'Circular-Loom';
-
-  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Book-cd7d2bcec649b1243839a15d5eb8f0a3.woff2') format('woff2');
-}
-
-@font-face {
-  font-weight: 500;
-  font-style:  normal;
-  font-family: 'Circular-Loom';
-
-  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Medium-d74eac43c78bd5852478998ce63dceb3.woff2') format('woff2');
-}
-
-@font-face {
-  font-weight: 700;
-  font-style:  normal;
-  font-family: 'Circular-Loom';
-
-  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Bold-83b8ceaf77f49c7cffa44107561909e4.woff2') format('woff2');
-}
-
-@font-face {
-  font-weight: 900;
-  font-style:  normal;
-  font-family: 'Circular-Loom';
-
-  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Black-bf067ecb8aa777ceb6df7d72226febca.woff2') format('woff2');
-}</style></head>
+  @include('layouts.head');
 
 <body class="home page-template page-template-page-welcome page-template-page-welcome-php page page-id-18 elementor-default elementor-kit-134 elementor-page elementor-page-18"><div class="parallax-mirror" style="visibility: hidden; z-index: -100; position: fixed; top: 0px; left: 0px; overflow: hidden; transform: translate3d(0px, 0px, 0px); height: 386px; width: 1903px;"><img class="parallax-slider" src="https://glbbestjapan.com/wp-content/uploads/2021/05/名称未設定のデザイン-47.png" style="transform: translate3d(0px, 0px, 0px); position: absolute; left: 0px; height: 713px; width: 1903px; max-width: none;"></div><div class="parallax-mirror" style="visibility: hidden; z-index: -100; position: fixed; top: 0px; left: 0px; overflow: hidden; transform: translate3d(0px, 0px, 0px); height: 384px; width: 1903px;"><img class="parallax-slider" src="https://glbbestjapan.com/wp-content/uploads/2021/05/名称未設定のデザイン-46.png" style="transform: translate3d(0px, 0px, 0px); position: absolute; left: 0px; height: 713px; width: 1903px; max-width: none;"></div>
 	
@@ -319,18 +60,18 @@ width: 100%;
       <div class="col-xs-120 no-padding">
         <div class="row text-left">
           <div class="text-center col-sm-40 mobile-mb-30" style="padding-left:30px;padding-right:30px;">
-            <div class="circleimages"><a href="https://glbbestjapan.com/outline/"><img src="https://glbbestjapan.com/wp-content/uploads/2021/05/forex-2.png"></a></div>            <h5 class="text-center section3-h5">会社概要</h5>
+            <div class="circleimages"><a href="{{ url('outline') }}"><img src="https://glbbestjapan.com/wp-content/uploads/2021/05/forex-2.png"></a></div>            <h5 class="text-center section3-h5">会社概要</h5>
             <div class="text-justify desc2">グローバルベストジャパンは、日本から最高のサービスを世界のクライアントに提供することを目指しています。</div>
-            <a class="read-more romaji mobile-mt-0" href="https://glbbestjapan.com/outline/">詳しく見る</a>          </div>
+            <a class="read-more romaji mobile-mt-0" href="{{ url('outline') }}">詳しく見る</a>          </div>
           <div class="text-center col-sm-40 mobile-mb-30" style="padding-left:30px;padding-right:30px;">
-            <div class="circleimages"><a href="https://glbbestjapan.com/business/"><img src="https://glbbestjapan.com/wp-content/uploads/2021/05/forex.png"></a></div>            <h5 class="text-center section3-h5">事業案内</h5>
+            <div class="circleimages"><a href="{{ url('business') }}"><img src="https://glbbestjapan.com/wp-content/uploads/2021/05/forex.png"></a></div>            <h5 class="text-center section3-h5">事業案内</h5>
             <div class="text-justify desc2">長年金融業界に携わった経験と豊富なITスキルをベースにクライアント様のご要望に柔軟にお応えします。</div>
-            <a class="read-more romaji mobile-mt-0" href="https://glbbestjapan.com/business/">詳しく見る</a>          </div>
+            <a class="read-more romaji mobile-mt-0" href="{{ url('business') }}">詳しく見る</a>          </div>
           <div class="text-center col-sm-40" style="padding-left:30px;padding-right:30px;">
-            <div class="circleimages"><a href="https://glbbestjapan.com/recruit/"><img src="https://glbbestjapan.com/wp-content/uploads/2020/01/380-380_3.jpg"></a></div>            <h5 class="text-center section3-h5">採用情報</h5>
+            <div class="circleimages"><a href="{{ url('recruit') }}"><img src="https://glbbestjapan.com/wp-content/uploads/2020/01/380-380_3.jpg"></a></div>            <h5 class="text-center section3-h5">採用情報</h5>
             <div class="text-justify desc2">私達とともに世界に向けて活躍する人材を求めています。<br>
 <br></div>
-            <a class="read-more romaji mobile-mt-0" href="https://glbbestjapan.com/recruit/">詳しく見る</a>          </div>
+            <a class="read-more romaji mobile-mt-0" href="{{ url('recruit') }}">詳しく見る</a>          </div>
         </div>
       </div>
     </div>
@@ -405,12 +146,6 @@ var doHeightAsViewport = function(){
 		$("#next-button").removeClass('opaque')
 
     $("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=jQuery(this.hash);if(e=e.length?e:jQuery("[name="+this.hash.slice(1)+"]"),e.length)return jQuery("html,body").animate({scrollTop:e.offset().top},1e3),!1}});
-
-    // setTimeout(function(){
-    //   if( $('.navbar-collapse').first().hasClass('in') ){
-    //     $(".navbar-toggle").trigger("click") 
-    //   }
-    // }, 300)
     
     if( $(window).width() < 1200 ){
       $(".navbar-toggle").click(function(){
